6adefce15eef66418b296530034d9434567d2642
FursykIvan <ivan.fursyk@gmail.com>
2025-01-13 03:01:45 +0200

    Solution

---
+++ app/serialize_strategy.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+import json
+import xml.etree.ElementTree as ET
+
+class SerializeStrategy(ABC):
+    @abstractmethod
+    def serialize(self, title: str, content: str) -> str:
+        pass
+
+
+class JsonSerialize(SerializeStrategy):
+    def serialize(self, title: str, content: str) -> str:
+        return json.dumps({"title": title, "content": content})
+
+
+class XmlSerialize(SerializeStrategy):
+    def serialize(self, title: str, content: str) -> str:
+        root = ET.Element("title")
+        title_elem = ET.SubElement(root, "title")
+        title_elem.text = title
+        content_elem = ET.SubElement(root, "content")
+        content_elem.text = content
+        return ET.tostring(root, encoding="unicode")
